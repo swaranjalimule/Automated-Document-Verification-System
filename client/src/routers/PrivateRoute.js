4aac1b59e3113952f8aa7dd0654414eb8fcbae40
swaranjalimule <92687269+swaranjalimule@users.noreply.github.com>
2024-06-30 22:34:49 +0530

    Add files via upload

---
+++ client/src/routers/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  // this is the PrivateRoute component
+  const { authenticated } = useSelector((state) => state.user); // this is the redux hook to get the user data from the redux store
+  return !authenticated ? <Navigate to="/" replace /> : children; // this is the ternary operator to check if the user is authenticated or not and return the children component if the user is authenticated or return the Navigate component to redirect the user to the home page if the user is not authenticated
+};
+
+export default PrivateRoute;
