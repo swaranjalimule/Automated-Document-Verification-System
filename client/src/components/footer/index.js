a10d112fdd60d380005f52e6c1620645d00101f9
swaranjalimule <92687269+swaranjalimule@users.noreply.github.com>
2024-06-30 22:28:11 +0530

    Add files via upload

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./index.scss";
+const footar = () => {
+  return (
+    <div className="footer">
+      <p>Copyright © 2024 AutoDocVerifier. All Rights Reserved.</p>
+      {/* <p>
+        Developed by:{" "}
+        <a href="https://linkedin.com/in/sakshiparkhe" target="_blank">
+          Sakshi Parkhe
+        </a>
+        ,{" "}
+        <a
+          href="https://www.linkedin.com/in/ujwala-more-155b2b1ba/"
+          target="_blank"
+        >
+          Ujwala More
+        </a>
+        ,{" "}
+        <a
+          href="https://www.linkedin.com/in/soham-chandratre-88651b254"
+          target="_blank"
+        >
+          Soham Chandratre
+        </a>
+        ,{" "}
+        <a
+          href="https://www.linkedin.com/in/chinmay-g-1b6503217"
+          target="_blank"
+        >
+          Chinmay Gangawane
+        </a>
+      </p> */}
+    </div>
+  );
+};
+
+export default footar;
